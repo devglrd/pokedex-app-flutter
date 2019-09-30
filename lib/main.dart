aa2bde35ef8f92cf06040c8341adbf503fe757c7
glrd <glrd.remi@gmail.com>
2019-09-30 03:06:56 +0200

    PokeDex App

---
+++ lib/main.dart
@@ -0,0 +1,110 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:pokemon/pokemon.dart';
+import 'package:pokemon/pokemonDetail.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Poke App",
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var url =
+      "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json";
+
+  PokeHub pokeHub;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  fetchData() async {
+    var res = await http.get(url);
+    var decodedJson = jsonDecode(res.body);
+    pokeHub = PokeHub.fromJson(decodedJson);
+    print(pokeHub.toJson());
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Poke App'),
+        backgroundColor: Colors.cyan,
+      ),
+      body: pokeHub == null
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : GridView.count(
+              crossAxisCount: 2,
+              children: pokeHub.pokemon
+                  .map((poke) => Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: InkWell(
+                          onTap: () => {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => PokemonDetail(
+                                          pokemon: poke,
+                                        )))
+                          },
+                          child: Hero(
+                            tag: poke.img,
+                            child: Card(
+                              elevation: 3.0,
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: <Widget>[
+                                  Container(
+                                    height: 100.0,
+                                    width: 100.0,
+                                    decoration: BoxDecoration(
+                                        image: DecorationImage(
+                                            image: NetworkImage(poke.img))),
+                                  ),
+                                  Text(
+                                    poke.name,
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ))
+                  .toList(),
+            ),
+      drawer: Drawer(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {},
+        backgroundColor: Colors.cyan,
+        child: Icon(Icons.refresh),
+      ),
+    );
+  }
+}
---
+++ lib/pokemonDetail.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:pokemon/pokemon.dart';
+
+class PokemonDetail extends StatelessWidget {
+  final Pokemon pokemon;
+  PokemonDetail({this.pokemon});
+
+  bodyWidget(BuildContext context) => Stack(
+        children: <Widget>[
+          Positioned(
+            height: MediaQuery.of(context).size.height / 1.5,
+            width: MediaQuery.of(context).size.width - 20,
+            left: 10.0,
+            top: MediaQuery.of(context).size.height * 0.1,
+            child: Card(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15)),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  SizedBox(
+                    height: 50,
+                  ),
+                  Text(
+                    pokemon.name,
+                    style: TextStyle(fontSize: 34),
+                  ),
+                  Text('Height : ${pokemon.height}'),
+                  Text('Wieght : ${pokemon.weight}'),
+                  Text('Types'),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: pokemon.type
+                          .map((t) => FilterChip(
+                                label: Text(t),
+                                onSelected: (b) {},
+                              ))
+                          .toList()),
+                  Text('Weakness'),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: pokemon.weaknesses
+                          .map((t) => FilterChip(
+                                label: Text(t),
+                                onSelected: (b) {},
+                              ))
+                          .toList()),
+                  Text('Next evolution'),
+                  pokemon.nextEvolution == null
+                      ? Text('No evolution')
+                      : Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: pokemon.nextEvolution
+                              .map((t) => FilterChip(
+                                    label: Text(t.name),
+                                    onSelected: (b) {},
+                                  ))
+                              .toList()),
+                ],
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.topCenter,
+            child: Hero(
+              tag: pokemon.img,
+              child: Container(
+                height: 150.00,
+                width: 150.0,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        fit: BoxFit.cover, image: NetworkImage(pokemon.img))),
+              ),
+            ),
+          )
+        ],
+      );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.cyan,
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.cyan,
+        title: Text('Detail of ${pokemon.name}'),
+      ),
+      body: bodyWidget(context),
+    );
+  }
+}
